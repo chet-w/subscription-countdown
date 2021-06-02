5178503e0d484977ff44d067659830b64bfec716
Chet Davidson <chethana96@gmail.com>
2021-06-03 09:54:35 +1200

    Add basic layout components

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,9 @@
+import { HeaderProps } from "./types"
+
+export function Header(props: HeaderProps) {
+  return (
+    <header>
+      <h1>he</h1> 
+    </header>
+  )
+}
---
+++ src/components/Main/index.tsx
@@ -0,0 +1,10 @@
+import { ReactElement } from 'react'
+import { MainProps } from './types'
+
+export function Main(props: MainProps): ReactElement {
+  return (
+    <div>
+      
+    </div>
+  )
+};
