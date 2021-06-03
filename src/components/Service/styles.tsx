5c2299f2639ea4f0ead6b1e4fe436f3cb16c716a
Chet Davidson <chethana96@gmail.com>
2021-06-03 15:42:10 +1200

    Turn service cards into buttons

--- src/components/Service/styles.tsx
+++ src/components/Service/styles.tsx
@@ -1,10 +1,18 @@
 import styled from "styled-components";
 
-export const Service = styled.article`
+export const Service = styled.button`
   padding: 1rem;
   background: #fafafa;
   width: 360px;
   border-radius: 1rem;
+  border: none;
+  display: block;
+  cursor: pointer;
+  transition: all 0.2s ease;
+
+  &:hover {
+    transform: scale(1.02);
+  }
 `;
 
 export const Image = styled.img`
