f36a509e6114ca0f05196746de72991e01e72029
Chet Davidson <chethana96@gmail.com>
2021-06-05 11:24:42 +1200

    Move gloabl styles to styled comoonents

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,14 @@ import { ThemeProvider } from "styled-components";
 import { AllSubscriptions } from "./components/AllSubscriptions";
 import { Layout } from "./components/Layout";
 import { UpcomingCharges } from "./components/UpcomingCharges";
-import { theme } from "./Theme";
-import "./App.css";
+import { theme } from "./theme/Theme";
+import { GlobalStyles } from "./theme/GlobalStyles";
 import { ModalProvider } from "./providers/ModalProvider";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
+      <GlobalStyles />
       <ModalProvider>
         <Layout>
           <UpcomingCharges />
