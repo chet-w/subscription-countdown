78a724b735a65bfb29205892b6fe3ac9c8954421
Chet Davidson <chethana96@gmail.com>
2021-06-05 13:21:47 +1200

    Create form component

---
+++ src/components/Form/index.tsx
@@ -0,0 +1,9 @@
+import { ReactElement } from "react";
+import { FormProps } from "./types";
+import * as S from "./styles";
+
+export function Form(props: FormProps): ReactElement {
+  const { children, ...otherProps } = props;
+
+  return <S.Form {...otherProps}>{children}</S.Form>;
+}
