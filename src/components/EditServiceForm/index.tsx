42c6c2e3e731f6388fd7c7cba0f892bd0eb1a089
Chet Davidson <chethana96@gmail.com>
2021-06-06 22:31:18 +1200

    Remove old deps, move types and testing deps to devDependencies

--- src/components/EditServiceForm/index.tsx
+++ src/components/EditServiceForm/index.tsx
@@ -1,5 +1,4 @@
 import { ReactElement } from "react";
-import { useForm, SubmitHandler } from "react-hook-form";
 import { Form } from "../Form";
 import { Input } from "../Input";
 import { Select } from "../Select";
@@ -13,30 +12,10 @@ const OCCURRANCE_OPTIONS: Option[] = [
 ];
 
 export function EditServiceForm(props: EditServiceFormProps): ReactElement {
-  const {
-    register,
-    formState: { errors },
-    handleSubmit
-  } = useForm<EditServiceFormInputs>({ mode: "all" });
-
-  const onSubmit: SubmitHandler<EditServiceFormInputs> = (data) =>
-    console.log(data);
-
   return (
-    <Form onSubmit={handleSubmit(onSubmit)}>
-      <Input
-        {...register("name", { required: true })}
-        label="Service name"
-        valid={!Boolean(errors.name)}
-        error="Service name can't be blank"
-      />
-      <Select
-        {...register("occurrance", { required: true })}
-        options={OCCURRANCE_OPTIONS}
-        label="Occurrance"
-        valid={!Boolean(errors.occurrance)}
-        error="Occurrance can't be blank"
-      />
+    <Form>
+      <Input label="Service name" />
+      <Select options={OCCURRANCE_OPTIONS} label="Occurrance" />
       <input type="submit" value="submit" />
     </Form>
   );
