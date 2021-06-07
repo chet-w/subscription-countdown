c5cae93bc9eaf2cad2de2d5bd877b77afd41321b
Chet Davidson <chethana96@gmail.com>
2021-06-07 20:04:26 +1200

    Extract fetching logic to a reusable hook

--- src/components/AllSubscriptions/index.tsx
+++ src/components/AllSubscriptions/index.tsx
@@ -1,32 +1,15 @@
-import { ReactElement, useEffect, useState } from "react";
-import { mapKeys, camelCase } from "lodash";
+import { ReactElement } from "react";
 import { AllSubscriptionProps } from "./types";
 import * as S from "./styles";
 import { Heading } from "../Heading";
 import { Service } from "../Service";
-import { Service as ServiceType } from "../../types/Service";
-import { dummy } from "../../dummy";
 import { useModal } from "../../providers/ModalProvider";
 import { EditServiceModal } from "../EditServiceModal";
+import { useServices } from "../../hooks/useServices";
 
 export function AllSubscriptions(props: AllSubscriptionProps): ReactElement {
-  // @ts-ignore
-  const [services, setServices] = useState<ServiceType[]>([]);
   const { openModal } = useModal();
-
-  useEffect(() => {
-    fetch("http://localhost:3000/services")
-      .then((res) => res.json())
-      .then((data) =>
-        setServices(
-          data.map((item: any) => mapKeys(item, (_, k) => camelCase(k)))
-        )
-      )
-      .catch((error) => {
-        console.error(error);
-        setServices(dummy);
-      });
-  }, []);
+  const services = useServices();
 
   return (
     <S.Section>
@@ -36,7 +19,9 @@ export function AllSubscriptions(props: AllSubscriptionProps): ReactElement {
           <Service
             {...service}
             key={JSON.stringify(service)}
-            onClick={() => openModal(<EditServiceModal service={service} />)}
+            onClick={() =>
+              openModal(<EditServiceModal serviceId={service.id} />)
+            }
           />
         ))}
       </S.ServiceContainer>
