6412107a94eb00fca934e22fb6a4ba49f5fb18b4
Chet Davidson <chethana96@gmail.com>
2021-06-07 20:03:57 +1200

    Update Service interface to show that the data has an id

--- src/dummy.ts
+++ src/dummy.ts
@@ -2,6 +2,7 @@ import { Service } from "./types/Service";
 
 export const dummy: Service[] = [
   {
+    id: 1,
     name: "Amazon Prime Video",
     image:
       "https://static.wikia.nocookie.net/logopedia/images/5/5c/Prime_Video_Icon_2020.svg",
@@ -11,6 +12,7 @@ export const dummy: Service[] = [
       "Sun Jun 20 2021 15:15:52 GMT+1200 (New Zealand Standard Time)"
   },
   {
+    id: 2,
     name: "Netflix",
     image:
       "https://www.nicepng.com/png/full/151-1510639_netflix-app-icon-png.png",
@@ -20,6 +22,7 @@ export const dummy: Service[] = [
       "Sun Jun 20 2021 15:15:52 GMT+1200 (New Zealand Standard Time)"
   },
   {
+    id: 3,
     name: "Spotify",
     image:
       "https://is5-ssl.mzstatic.com/image/thumb/Purple125/v4/31/1e/b0/311eb00e-1686-3c47-bd5e-d4c743c9762b/AppIcon-0-0-1x_U007emarketing-0-0-0-6-0-0-sRGB-0-0-0-GLES2_U002c0-512MB-85-220-0-0.png/230x0w.webp",
@@ -29,6 +32,7 @@ export const dummy: Service[] = [
       "Sun Jun 20 2021 15:15:52 GMT+1200 (New Zealand Standard Time)"
   },
   {
+    id: 4,
     name: "Lastpass",
     image:
       "https://is2-ssl.mzstatic.com/image/thumb/Purple115/v4/7e/3f/90/7e3f90dc-284a-019f-092e-ac758a8d22ee/AppIcon_distribution-0-0-1x_U007emarketing-0-0-0-6-0-0-sRGB-0-0-0-GLES2_U002c0-512MB-85-220-0-0.png/230x0w.webp",
@@ -38,6 +42,7 @@ export const dummy: Service[] = [
       "Sun Jun 20 2021 15:15:52 GMT+1200 (New Zealand Standard Time)"
   },
   {
+    id: 5,
     name: "Apple Arcade",
     image:
       "https://static.wikia.nocookie.net/logopedia/images/c/cb/ZZZZFlattenedImage-1.1_Normal_4.png",
@@ -47,6 +52,7 @@ export const dummy: Service[] = [
       "Sun Jun 20 2021 15:15:52 GMT+1200 (New Zealand Standard Time)"
   },
   {
+    id: 6,
     name: "iCloud",
     image:
       "https://www.apple.com/v/icloud/z/images/overview/header_drive__gl7rt5lk2emq_large_2x.png",
@@ -56,6 +62,7 @@ export const dummy: Service[] = [
       "Sun Jun 20 2021 15:15:52 GMT+1200 (New Zealand Standard Time)"
   },
   {
+    id: 7,
     name: "Hello Fresh",
     image:
       "https://play-lh.googleusercontent.com/FCm6Y99r1v7NePwvj0Yu8qzPkXysU4j_BnPc4fpWsdOkQ21HyF33GiZEOHVm80EfzkU=s360-rw",
@@ -65,6 +72,7 @@ export const dummy: Service[] = [
       "Sun Jun 20 2021 15:15:52 GMT+1200 (New Zealand Standard Time)"
   },
   {
+    id: 8,
     name: "Audible",
     image:
       "https://play-lh.googleusercontent.com/3B91t9YONRYkJz1dq381fAN3zxp1hnTx98Rhve6a8RT_dzqZGi2pTJKzQOoCltPSOWM=s360-rw",
@@ -74,6 +82,7 @@ export const dummy: Service[] = [
       "Sun Jun 20 2021 15:15:52 GMT+1200 (New Zealand Standard Time)"
   },
   {
+    id: 9,
     name: "Microsoft Office",
     image:
       "https://cdn.jim-nielsen.com/ios/512/microsoft-office-2020-02-26.png",
