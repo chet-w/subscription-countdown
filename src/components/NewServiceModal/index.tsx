9c0c43e55b9ba8f26bcaef5ad15fc84f9b3a91e6
Chet Davidson <chethana96@gmail.com>
2021-06-19 20:14:00 +1200

    Remove unused import

--- src/components/NewServiceModal/index.tsx
+++ src/components/NewServiceModal/index.tsx
@@ -1,12 +1,9 @@
 import { ReactElement } from "react";
-import { useModal } from "../../providers/ModalProvider";
 import { Modal } from "../Modal";
 import { ServiceForm } from "../ServiceForm";
 import { NewServiceModalProps } from "./types";
 
 export function NewServiceModal(props: NewServiceModalProps): ReactElement {
-  const { closeModal } = useModal();
-
   return (
     <Modal id="new-service" title="New Service">
       <ServiceForm formButtonLabels={["Cancel", "Create Service"]} />
