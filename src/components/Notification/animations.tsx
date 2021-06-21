18f216732954e00a22a6e64dbb5cef8c08a7aaa2
Chet Davidson <chethana96@gmail.com>
2021-06-21 17:53:55 +1200

    Change animation style for notification

--- src/components/Notification/animations.tsx
+++ src/components/Notification/animations.tsx
@@ -1,18 +1,12 @@
-import { Variants, Transition } from "framer-motion";
+import { Variants } from "framer-motion";
 
 export const variants: Variants = {
   hidden: {
-    x: -300,
+    y: 100,
     opacity: 0
   },
   active: {
-    x: 0,
+    y: 0,
     opacity: 1
   }
-};
-
-export const transition: Transition = {
-  damping: 20,
-  stiffness: 180,
-  type: "spring"
 };
--- src/components/Notification/index.tsx
+++ src/components/Notification/index.tsx
@@ -1,14 +1,13 @@
 import { ReactElement } from "react";
 import { NotificationProps } from "./types";
 import * as S from "./styles";
-import { variants, transition } from "./animations";
+import { variants } from "./animations";
 
 export function Notification(props: NotificationProps): ReactElement {
   return (
     <S.Notification
       variants={variants}
       variant={props.variant}
-      transition={transition}
       initial="hidden"
       animate="active"
       exit="hidden"
